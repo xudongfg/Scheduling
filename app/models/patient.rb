ede68a9d8d3f5db15c5c009834a6ed58f82c3cbc
xudongfg <frank.fengxd@gmail.com>
2016-03-07 17:45:18 -0800

    Implement null user name validation and user deletion dependency with appointments.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,5 @@
 class Patient < ActiveRecord::Base
-	has_many :appointments
+	has_many :appointments, dependent: :destroy
+	validates :first_name, presence: true
+	validates :last_name, presence: true
 end
